976287ff9c088f6f959bdb607ad0ec579001d326
Milan Gotera <milangotera@gmail.com>
2020-06-26 09:08:49 -0500

    Adding md5

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const express    = require("express");
 const bodyParser = require("body-parser");
 
+require('./config/mongoose');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,7 +12,7 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
   next();
 });
@@ -23,5 +25,5 @@ app.use('/', router);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
-    console.log('Press Ctrl+C to quit.');
+    console.log('Press Ctrl+C to quit');
 });
